71fe1b43e39e9433496050e6c153a5f36c003510
GGost <gabriel.restrepoi@udea.edu.co>
2020-11-26 19:55:10 -0500

    SE AGREGO LA FUNCION LA PARTE DE CARGAR DOCUMENTO PERO EN EL main.cpp SE HACE UNA ADVERTENDIA DE QUE DEVE TENER EL CODIGO PARA QUE FUNCIONE DE MANERA CORRECTA.

--- FUNCIONES.cpp
+++ FUNCIONES.cpp
@@ -15,12 +15,13 @@ vector<string> rut={""};
 vector<int> cost= {90000000};
 int j;
 
-string leer(){
+string leer(string nombre_archivo){
     string data;
     string texto;
     // Abre el archivo en modo lectura
     ifstream infile;
-    string direccion="../practica_4/rutas/rutas.txt";
+    string direccion="../practica_4/rutas/";
+    direccion+=nombre_archivo;
     // Se pone de manera explicita la ruta relativa donde se encuentra el archivo
     infile.open(direccion);
 
@@ -92,6 +93,39 @@ archivo donde se tienen guardadas las rutas*/
     Guardar.close();
 
 }
+void AgregarRouter2(string doc){
+    /*esta funcion tiene como objetivo agragar todos los routers que el usuario desee agregar al
+archivo donde se tienen guardadas las rutas*/
+    string router,k;
+    ofstream Guardar;
+    k="../practica_4/rutas/";
+    k+=doc;
+    Guardar.open(k,ios::app);
+    while (router!="-1") {
+        cout<<"PORFAVOR INGRESELO DE LA FORMA "
+              "<NUEVO><VIEJO>-<COSTO> (CON UN ENTER O SALTO DE LINEA) "<<endl<<"Y VISEVERSA "
+              "<VIEJO><NUEVO>-<COSTO> (CON UN ENTER O SALTO DE LINEA) "
+              ";EJEMPLO <AZ-9> Y <ZA-9>."<<endl;
+        cout<<"********************************************"<<endl;
+        cout<<"*      INGRESE EL NUEVO ROUTER ASI         *"<<endl;
+        cout<<"********************************************"<<endl;
+        cout<<"*                                          *"<<endl;
+        cout<<"*      <ROUTER 1><ROUTER 2>-<COSTO>        *"<<endl;
+        cout<<"*                                          *"<<endl;
+        cout<<"********************************************"<<endl;
+        cout<<"*             O -1 PARA SALIR              *"<<endl;
+        cout<<"********************************************"<<endl;
+        cin>>router;
+        if(router!="-1"){
+            router+=".";
+            Guardar<<router<<endl;
+            system("CLS");
+        }
+    }
+
+    Guardar.close();
+
+}
 void BorrarRouter(){
     /*esta función tiene el propósito de eliminar el Reuter elegido por el usuario,
 se hace siguiendo estos pasos
@@ -108,7 +142,7 @@ esta el Reuter que dio el usuario se elimina dicho componente del mapa
     temp_txt.open("../practica_4/rutas/temporal.txt",ios::app);
     map<string,short> enlaces;
     map<string,short>::iterator it;
-    string sacado=leer();
+    string sacado=leer("rutas.txt");
     pos =sacado.find_first_of('.');
     for(unsigned int i=0;i<sacado.length();i++){
         if(i<pos){
@@ -194,6 +228,116 @@ esta el Reuter que dio el usuario se elimina dicho componente del mapa
             Guardar.close();
             remove("../practica_4/rutas/rutas.txt");
             rename("../practica_4/rutas/temporal.txt","../practica_4/rutas/rutas.txt");
+            cout<<"EL ROUTER "<<router<<" fue eliminado"<<endl;
+            _sleep(3500);
+            system("CLS");
+        }
+    }
+}
+void BorrarRouter2(string doc){
+    /*esta función tiene el propósito de eliminar el Reuter elegido por el usuario,
+se hace siguiendo estos pasos
+1) extraer los enlaces y ubicarlos en un mapa
+2) buscar con ayuda de un iterador si el Reuter que dio el usuario está en el mapa
+en la parte de key en la cual están los nombres de los Reuters conectados y si se encuentra que
+esta el Reuter que dio el usuario se elimina dicho componente del mapa
+3)se agrega a un archivo temporal la información editada*/
+    int x=0,pos,posinicio=0,posaux1=0,posaux2;
+    string temp1,temp2,txtau,neu,temporal,txtguia,k;char router,ru=' ';
+    ofstream Guardar;
+    ofstream temp_txt;
+    k="../practica_4/rutas/";
+    k+=doc;
+    Guardar.open(k,ios::app);
+    temp_txt.open("../practica_4/rutas/temporal.txt",ios::app);
+    map<string,short> enlaces;
+    map<string,short>::iterator it;
+    string sacado=leer(doc);
+    pos =sacado.find_first_of('.');
+    for(unsigned int i=0;i<sacado.length();i++){
+        if(i<pos){
+            txtguia+=sacado.at(i);
+        }
+
+    }
+    txtguia+=".";
+    temp_txt<<txtguia<<endl;
+    while(true){
+        for(int i=posinicio;i < pos;i++){
+            if(posinicio==0){
+                for(unsigned int i=0;i<sacado.length();i++){
+                    if(i>pos){
+                        temp2+=sacado.at(i);
+                    }
+
+                }sacado=temp2;
+                temp2="";
+                pos=0;
+                break;
+            }
+            else if(sacado.at(i)!=' '){
+                temp1+=sacado.at(i);
+            }
+        }
+        if (temp1!=""){
+            posaux1=temp1.find_first_of(",");
+            for(unsigned int i=0;i<temp1.length();i++){
+                if(i>posaux1){
+                    temp2+=temp1.at(i);
+                }else if(temp1.at(i)!='-'){
+                    txtau+=temp1.at(i);
+                }
+            }
+            double num_double = std::stod(temp2);
+            enlaces[txtau]=num_double;
+        }
+
+        posinicio=pos+1;
+
+        for(unsigned int i=0;i<sacado.length();i++){
+            if(i>pos){
+                neu+=sacado.at(i);
+            }
+
+        }
+        posaux2=neu.find_first_of('.');
+        pos +=posaux2+1;
+        neu="";
+        temp2="";
+        temp1="";
+        txtau="";
+        if(posaux2==-1){
+            break;
+        }
+    }
+    while (router!='1') {
+        cout<<"********************************************"<<endl;
+        cout<<"*      INGRESE EL ROUTER A ELIMINAR        *"<<endl;
+        cout<<"********************************************"<<endl;
+        cout<<"*                                          *"<<endl;
+        cout<<"*                <ROUTER>                  *"<<endl;
+        cout<<"*                                          *"<<endl;
+        cout<<"********************************************"<<endl;
+        cout<<"*             O 1 PARA SALIR               *"<<endl;
+        cout<<"********************************************"<<endl;
+        cin>>router;
+        if(router!='1'){
+            if(ru!=router){
+                for(it=enlaces.begin();it!=enlaces.end();it++){
+                    if(it->first[0]==router || it->first[1]==router){}
+                    else{temporal+=it->first+"-" + to_string(it->second)+".";
+                        temp_txt<<temporal<<endl;
+                        temporal="";
+                    }
+
+                }
+
+            }
+            ru=router;
+            temp_txt.close();
+            Guardar.close();
+            remove("../practica_4/rutas/topologia.txt");
+            rename("../practica_4/rutas/temporal.txt","../practica_4/rutas/topologia.txt");
             cout<<"EL ROUTER "<<router<<" fue eliminado"<<endl;
             _sleep(3500);
             system("CLS");
@@ -287,7 +431,7 @@ void BusquedaRutas(){
         int x=0,pos,posinicio=0,posaux1=0,posaux2;
         string temp1,temp2,txtau,neu;
         map<string,short> enlaces;
-        string sacado=leer();
+        string sacado=leer("rutas.txt");
         pos =sacado.find_first_of('.');
         while(true){
             for(int i=posinicio;i < pos;i++){
@@ -348,62 +492,84 @@ void BusquedaRutas(){
         }
        }
 }
+void BusquedaRutas2(string doc){
+    char origen,destino;
+    string c,ruta;short costo;
+    cout<<"********************************************"<<endl;
+    cout<<"*      INGRESE DE LA DIGUIENTE FORMA       *"<<endl;
+    cout<<"********************************************"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"*          <ROUTER 1><ROUTER 1>            *"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"********************************************"<<endl;
+    cout<<"*             O -1 PARA SALIR               *"<<endl;
+    cout<<"********************************************"<<endl;
+    cin>>c;
+    if (c!="-1"){
+        origen=c[0];destino=c[1];ori=c[0];
+        int x=0,pos,posinicio=0,posaux1=0,posaux2;
+        string temp1,temp2,txtau,neu;
+        map<string,short> enlaces;
+        string sacado=leer(doc);
+        pos =sacado.find_first_of('.');
+        while(true){
+            for(int i=posinicio;i < pos;i++){
+                if(posinicio==0){
+                    for(unsigned int i=0;i<sacado.length();i++){
+                        if(i>pos){
+                            temp2+=sacado.at(i);
+                        }
 
-void Mapa(){
-    int x=0,pos,posinicio=0,posaux1=0,posaux2;
-    string temp1,temp2,txtau,neu;
-    map<string,short> enlaces;
-    string sacado=leer();
-    pos =sacado.find_first_of('.');
-    while(true){
-        for(int i=posinicio;i < pos;i++){
-            if(posinicio==0){
-                for(unsigned int i=0;i<sacado.length();i++){
-                    if(i>pos){
-                        temp2+=sacado.at(i);
-                    }
-
-                }sacado=temp2;
-                temp2="";
-                pos=0;
-                break;
-            }
-            else if(sacado.at(i)!=' '){
-                temp1+=sacado.at(i);
-            }
-        }
-        if (temp1!=""){
-            posaux1=temp1.find_first_of("-");
-            for(unsigned int i=0;i<temp1.length();i++){
-                if(i>posaux1){
-                    temp2+=temp1.at(i);
-                }else if(temp1.at(i)!='-'){
-                    txtau+=temp1.at(i);
+                    }sacado=temp2;
+                    temp2="";
+                    pos=0;
+                    break;
+                }
+                else if(sacado.at(i)!=' '){
+                    temp1+=sacado.at(i);
                 }
             }
-            double num_double = std::stod(temp2);
-            enlaces[txtau]=num_double;
-        }
+            if (temp1!=""){
+                posaux1=temp1.find_first_of("-");
+                for(unsigned int i=0;i<temp1.length();i++){
+                    if(i>posaux1){
+                        temp2+=temp1.at(i);
+                    }else if(temp1.at(i)!=','){
+                        txtau+=temp1.at(i);
+                    }
+                }
+                double num_double = std::stod(temp2);
+                enlaces[txtau]=num_double;
+            }
 
-        posinicio=pos+1;
+            posinicio=pos+1;
 
-        for(unsigned int i=0;i<sacado.length();i++){
-            if(i>pos){
-                neu+=sacado.at(i);
+            for(unsigned int i=0;i<sacado.length();i++){
+                if(i>pos){
+                    neu+=sacado.at(i);
+                }
+
+            }
+            posaux2=neu.find_first_of('.');
+            pos +=posaux2+1;
+            neu="";
+            temp2="";
+            temp1="";
+            txtau="";
+            if(posaux2==-1){
+                break;
             }
 
         }
-        posaux2=neu.find_first_of('.');
-        pos +=posaux2+1;
-        neu="";
-        temp2="";
-        temp1="";
-        txtau="";
-        if(posaux2==-1){
-            break;
-        }
+        map<string,short>::iterator it1;
 
-    }
+       fu(origen, destino, enlaces,ruta,costo,r);
+        if(j==0){
+            cout<<"el costo minimo es: "<<cost[0]<<endl;
+        }else if(j==1){
+            cout<<"la ruta minima es: "<<rut[0]<<endl;
+        }
+       }
 }
 bool Menu(){
     int opcion;
@@ -453,3 +619,95 @@ bool Menu(){
     }
 
 }
+bool Menu2(){
+    int opcion;
+    string doc;
+    cout<<"INGRESE NOMBRE DEL DOCUMENTO TERMINADO EN .TXT"<<endl;
+    cin>>doc;
+    system("CLS");
+    cout<<"********************************************"<<endl;
+    cout<<"*             QUE QUIERE HECER             *"<<endl;
+    cout<<"********************************************"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"*   1.AGREGAR ROUTER                       *"<<endl;
+    cout<<"*   2.BORRAR ROUTER                        *"<<endl;
+    cout<<"*   3.COSTO ENVIO <R1-R2>                  *"<<endl;
+    cout<<"*   4.RUTA ENVIO <Ri-R2>                   *"<<endl;
+    cout<<"*   5.SALIR                                *"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"********************************************"<<endl;
+    cin>>opcion;
+    switch (opcion) {
+    case 1:
+        system("CLS");
+        AgregarRouter2(doc);
+        break;
+    case 2:
+        system("CLS");
+        cout<<"INGRESE NOMBRE DEL DOCUMENTO ";
+        cin>>doc;
+        BorrarRouter2(doc);
+        break;
+    case 3:
+        system("CLS");
+        j=0;
+
+        BusquedaRutas2(doc);
+        _sleep(2000);
+        return true;
+    case 4:
+        system("CLS");
+        j=1;
+
+        BusquedaRutas2(doc);
+        _sleep(2000);
+        return true;
+    case 5:
+        cout<<"********************************************"<<endl;
+        cout<<"*           QUE TENGA BUEN DIA             *"<<endl;
+        cout<<"********************************************"<<endl;
+        return false;
+    default:
+        cout<<"********************************************"<<endl;
+        cout<<"*             !NO ESCOGIO NADA¡            *"<<endl;
+        cout<<"********************************************"<<endl;
+        break;
+    }
+
+}
+bool Menu_(){
+    int opcion;
+    bool x;
+    cout<<"********************************************"<<endl;
+    cout<<"*             QUE QUIERE HECER             *"<<endl;
+    cout<<"********************************************"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"*   1.TRABAJAR CON RUTAS.TXT               *"<<endl;
+    cout<<"*   2.CARGAR DOCUMENTO                     *"<<endl;
+    cout<<"*   3.SALIR                                *"<<endl;
+    cout<<"*                                          *"<<endl;
+    cout<<"********************************************"<<endl;
+    cin>>opcion;
+    switch (opcion) {
+    case 1:
+        system("CLS");
+        x=Menu();
+        return x;
+    case 2:
+        system("CLS");
+        x=Menu2();
+        return x;
+    case 3:
+        cout<<"********************************************"<<endl;
+        cout<<"*           QUE TENGA BUEN DIA             *"<<endl;
+        cout<<"********************************************"<<endl;
+        return false;
+    default:
+        cout<<"********************************************"<<endl;
+        cout<<"*             !NO ESCOGIO NADA¡            *"<<endl;
+        cout<<"********************************************"<<endl;
+        break;
+    }
+}
+
+
--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 /*si se quiere editar las rutas por un archivo de texto editarla por favor en el
 archivo llamado rutas.txt ubicado en la carpeta llamada rutas siguiendo las indicaciones que se
 indican en el archivo rutas.txt*/
+/*ingresar el documento topologia.txt en el archivo llamado rutas y dentro del documento topologia.txt
+ porfavor que halla un comentario inicial terminando en "." y que cada ruta termine tambien en "."
+ ej:
+  HOLA.
+  AB,5.
+  BA,5.
+*/
 
 
 
@@ -12,7 +19,7 @@ int main()
     bool x=true;
     while(x){
         system("CLS");
-        x=Menu();}
+        x=Menu_();}
     return 0;
 }
 
